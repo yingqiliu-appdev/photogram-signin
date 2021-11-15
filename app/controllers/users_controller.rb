f4ac56885bbaa0f0d1ff6476e07bdb780380212d
yingqi0408 <ying_liu@chicagobooth.edu>
2021-11-15 17:35:44 +0000

    up until minute 45

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,40 @@
 class UsersController < ApplicationController
+
+  def authenticate
+    # get username and password input
+    un = params.fetch("input_username")
+    pw = params.fetch("input_password")
+
+    # look up the record from the db matching username
+    user = User.where({ :username => un}).at(0)
+
+    # if there's no record, redirect back to sign in form
+    if user == nil
+      redirect_to("/user_sign_in", { :alert => "No one by that name 'round these parts"})
+    else
+      user.authenticate(pw)
+      redirect_to("/")
+    render({ :plain => "hi" })
+  end
+
+  def toast_cookies
+    reset_session
+
+    redirect_to("/", { :notice => "See ya later!"})
+  end
+
+  def new_registration_form
+
+    render({ :template => "users/signup_form.html.erb"})
+
+  end
+
+  def new_session_form
+
+    render({ :template => "users/signin_form.html.erb"})
+
+  end
+
   def index
     @users = User.all.order({ :username => :asc })
 
@@ -16,10 +52,17 @@ class UsersController < ApplicationController
     user = User.new
 
     user.username = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
 
-    user.save
+    save_status = user.save
 
-    redirect_to("/users/#{user.username}")
+    if save_status == true
+      session.store(:user_id, user.id)
+      redirect_to("/users/#{user.username}", { :notice => "Welcome, " + user.username + "!"})
+    else
+      redirect_to("/user_sign_up", { :alert => user.errors.full_messages.to_sentence })
+    end
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   # User routes
 
+  get("/user_sign_up", { :controller => "users", :action => "new_registration_form"})
+  get("/user_sign_in", { :controller => "users", :action => "new_session_form"})
+  get("/user_sign_out", { :controller => "users", :action => "toast_cookies"})
+  post("/verify_credentials", { :controller => "users", :action => "authenticate"})
+
   # CREATE
   get("/insert_user_record", {:controller => "users", :action => "create" })
 
@@ -41,5 +46,7 @@ Rails.application.routes.draw do
   # DELETE
 
   get("/delete_comment/:the_comment_id", { :controller => "comments", :action => "destroy"})
+
+
 
 end
